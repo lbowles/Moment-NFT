f164aae378a37923f069119ed72024fdcbf8d73e
lbowles <lspbowles@gmail.com>
2022-03-30 10:20:41 +0200

    creating deployment files

--- backend/contracts/momentNFT.sol
+++ backend/contracts/momentNFT.sol
@@ -9,23 +9,42 @@ contract momentNFT is ERC721URIStorage {
   using SafeMath for uint256;
 
   uint public tokenCounter; 
+  event CreatedMomentNFT(uint256 indexed tokenId, string tokenURI);
 
   constructor() ERC721 ("Moment NFT", "momentNFT") {
     tokenCounter = 0 ;
   }
 
-  function create(string memory svg) public {
+  function create(string memory _svg) public {
     _safeMint(msg.sender, tokenCounter);
-    string memory imageURI = svgToImageURI(svg);
+    string memory imageURI = svgToImageURI(_svg);
+    string memory tokenURI = formatTokenURI(imageURI); 
+    _setTokenURI(tokenCounter, tokenURI);
+    emit CreatedMomentNFT(tokenCounter, tokenURI);
     tokenCounter = tokenCounter.add(1) ; 
   }
 
-  function svgToImageURI(string memory svg) public pure returns (string memory){
+  function svgToImageURI(string memory _svg) public pure returns (string memory){
     string memory baseURI = "data:image/svg+xml;base64,";
-    string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))))  ; 
+    string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(_svg))))  ; 
     string memory imageURI = string(abi.encodePacked(baseURI,svgBase64Encoded));
     return imageURI;
   }
 
+  function formatTokenURI(string memory _imageURI)public pure returns(string memory){
+
+    string memory baseURL = "data:application/json;base64";
+    return string(abi.encodePacked(
+      baseURL,
+      Base64.encode(
+        bytes(abi.encodePacked(
+          '{"name":"Moment NFT"',
+          '"description": "Fully on-chain clock NFT that shows you the current time"',
+          '"time-zone":""',
+          '"image":"',_imageURI,'"}')
+        )
+      )
+    ));
+  }
   
 }
---
+++ backend/deploy/01_deploy_momentnft.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+
+module.exports = async({
+  getNamedAccounts,
+  deployments,
+  getChainId
+}) => {
+  const {deploy, log} = deployments
+  const {deployer} = await getNamedAccounts()
+  const chainId = await getChainId()
+
+  log("[][][]][][][][][][][][][][][][][][]")
+  const momentNFT = await deploy("momentNFT", {
+    from: deployer,
+    log: true 
+  })
+  log('You have deployed the contract to ' +momentNFT.address )
+  let file = "./backend/img/clock.svg"
+  let svg = fs.readFileSync(file, {encoding:"utf-8"})
+
+  const momentNFTContract = await ethers.getContractFactory("momentNFT")
+
+}
