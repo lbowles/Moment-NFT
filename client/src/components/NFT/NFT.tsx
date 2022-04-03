3d9728d5720c84a3df66ded743dcacee691e3616
lbowles <lspbowles@gmail.com>
2022-04-03 15:48:50 +0200

    mock SVG sizing

--- client/src/components/NFT/NFT.tsx
+++ client/src/components/NFT/NFT.tsx
@@ -28,7 +28,7 @@ setInterval(runClock, 1000);
 export const NFT = ({timeZoneHour,timeZoneMin}: NFTProps) => {
   return <div className={style.nft}>
     <div className={style.container}>
-      <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="100%" height="100%" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="400" height="400" fill="white" />
       <circle cx="200" cy="200" r="147" stroke="black" stroke-width="6" />
       <circle cx="200" cy="200" r="145" fill="black" stroke="#393939" stroke-width="2" />
